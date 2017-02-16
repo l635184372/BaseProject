4ae2f6961e4e5ce8ce8b5f1bfe1de3c0580034bf
liangqx <lwma6148135@126.com>
2017-02-16 16:06:03 +0800

    修改

--- src/main/java/com/base/cn/controller/BaseController.java
+++ src/main/java/com/base/cn/controller/BaseController.java
@@ -2,7 +2,11 @@ package com.base.cn.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 类名: BaseController </br>
@@ -12,9 +16,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * 发布版本：V1.0  </br>
  */
 @Controller
-@RequestMapping(value = "/base")
+@RequestMapping(value = "/base/")
 public class BaseController {
 
     private Logger logger = Logger.getLogger(BaseController.class);
 
+    @RequestMapping(value = "index")
+    public String toIndex(HttpServletRequest request, HttpServletResponse response, Model model) {
+        return "jsp/index";
+    }
+
+    @RequestMapping(value = "dashboard_1")
+    public String dashboard_1(HttpServletRequest request, HttpServletResponse response, Model model) {
+        return "jsp/dashboard_1";
+    }
+
+    @RequestMapping(value = "dashboard_2")
+    public String toDashboard_2(HttpServletRequest request, HttpServletResponse response, Model model) {
+        return "jsp/dashboard_2";
+    }
+    @RequestMapping(value = "dashboard_3")
+    public String toDashboard_3(HttpServletRequest request, HttpServletResponse response, Model model) {
+        return "jsp/dashboard_3";
+    }
+    @RequestMapping(value = "dashboard_4")
+    public String toDashboard_4(HttpServletRequest request, HttpServletResponse response, Model model) {
+        return "jsp/dashboard_4";
+    }
+    @RequestMapping(value = "dashboard_5")
+    public String toDashboard_5(HttpServletRequest request, HttpServletResponse response, Model model) {
+        return "jsp/dashboard_5";
+    }
+    @RequestMapping(value = "layouts")
+    public String toLayouts(HttpServletRequest request, HttpServletResponse response, Model model) {
+        return "jsp/layouts";
+    }
 }
---
+++ src/main/webapp/WEB-INF/views/include/taglib.jsp
@@ -0,0 +1,11 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--
+  Created by IntelliJ IDEA.
+  User:
+  Date: 2017/2/16
+  Time: 8:17
+  To change this template use File | Settings | File Templates.
+--%>
+<c:set var="ctx" value="${pageContext.request.contextPath}"/>
+<c:set var="sysPage" value="${pageContext.request.contextPath}/base"/>
+
